5cc6ffeb5933cae82de43609201d8858ce54c08b
perry.davis242@protonmail.com <perry.davis242@protonmail.com>
2021-12-12 14:11:22 -0500

    updated html generator

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const name = profileDataArgs[0];
 const github = profileDataArgs[1];
@@ -59,5 +60,11 @@ const generatePage = (name, github) => {
     </html>
     `;
 };
-console.log(name, github);
-console.log(generatePage(name, github));
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
