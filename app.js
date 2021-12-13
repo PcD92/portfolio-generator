84098ca77fc407e3638868469192d21368b1753c
perry.davis242@protonmail.com <perry.davis242@protonmail.com>
2021-12-13 18:07:34 -0500

    created page-template js with page generator

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 // const profileDataArgs = process.argv.slice(2);
 
 // const animalArray = ['dog', 'cat', 'pig'];
@@ -42,24 +42,7 @@ const github = profileDataArgs[1];
 //  console.log(message);
 //  console.log(sum);
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+
 // console.log(name, github);
 // console.log(generatePage(name, github));
 
