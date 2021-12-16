a889f4089141ede4a5ee0611cd0cd90462765bc2
perry.davis242@protonmail.com <perry.davis242@protonmail.com>
2021-12-16 00:33:36 -0500

    added inquirer method and questions for CLI

--- app.js
+++ app.js
@@ -1,53 +1,26 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
 // const profileDataArgs = process.argv.slice(2);
 
-// const animalArray = ['dog', 'cat', 'pig'];
+// console.log(profileDataArgs);
 
-// animalArray.push('cow');
-
-// const personObj = {
-//     name: 'Lernantino',
-//     age: 99
-// };
-
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-//  const printProfileData = profileDataArr => {
-//      // This...
-//      for (let i = 0; i < profileDataArr.length; i += 1) {
-//          console.log(profileDataArr[i]);
-//      }
-
-//      console.log('q================P');
-
-//      // Is the same as this...
-//      profileDataArr.forEach(profileItem => console.log(profileItem));
-//  };
-//  printProfileData(profileDataArgs);
-
-//  const message = 'Hello Node!';
-
-//  if (true === true) {
-//      const message = 'Hello ES6!';
-//      let sum = 5;
-//      sum += 10;
-//      console.log(message);
-//      console.log(sum);
-//  }
-
-//  console.log(message);
-//  console.log(sum);
-
+// const [name, github] = profileDataArgs;
 
 // console.log(name, github);
-// console.log(generatePage(name, github));
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'Name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
